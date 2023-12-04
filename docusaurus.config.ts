48be59fa436c4136c853d37492bd10a2a2eca432
Abdallah Shaban <ashaban@teo.cloud>
2023-12-04 13:58:24 -0800

    chore: Update css for input

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
         tagName: "meta",
         attributes: {
           name: "viewport",
-          content: "width=device-width, initial-scale=1.0",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
         },
       },
     ],
