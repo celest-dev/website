6e31eec8f7a23dcde47c60480f4615bf68b2989a
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-12-04 16:10:24 -0700

    chore: Update title (#21)
    For `<title>` and Twitter OG.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import LandingPage from "../components/LandingPage/LandingPage";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title} - The Flutter cloud platform`}
       description="The Flutter cloud platform"
     >
       <main>
