1ac819e3a03840bf89408a7e4b51eacdf864196d
Abdallah Shaban <ashaban@teo.cloud>
2023-12-04 12:55:10 -0800

    chore: Update styling (#11)
    Chore (docs): Update CSS changes to the website

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,58 +1,64 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Celest',
-  tagline: 'A managed cloud platform to build full-stack Flutter and Dart apps',
-  favicon: 'img/logo.png',
+  title: "Celest",
+  tagline: "A managed cloud platform to build full-stack Flutter and Dart apps",
+  favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: 'https://celest.dev',
+  url: "https://celest.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
-  organizationName: 'celest-dev',
-  projectName: 'website',
-  deploymentBranch: 'gh-pages',
+  organizationName: "celest-dev",
+  projectName: "website",
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
-        docs: process.env.NODE_ENV === 'production' ? false : {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: process.env.NODE_ENV === 'production' ? false : {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs:
+          process.env.NODE_ENV === "production"
+            ? false
+            : {
+                sidebarPath: "./sidebars.ts",
+                // Please change this to your repo.
+                // Remove this to remove the "edit this page" links.
+                editUrl:
+                  "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+              },
+        blog:
+          process.env.NODE_ENV === "production"
+            ? false
+            : {
+                showReadingTime: true,
+                // Please change this to your repo.
+                // Remove this to remove the "edit this page" links.
+                editUrl:
+                  "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+              },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
         gtag: {
-          trackingID: 'G-1FVDBFPGXN',
+          trackingID: "G-1FVDBFPGXN",
           anonymizeIP: true,
         },
       } satisfies Preset.Options,
@@ -60,54 +66,62 @@ const config: Config = {
   ],
 
   themeConfig: {
+    head: [
+      {
+        tagName: "meta",
+        attributes: {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0",
+        },
+      },
+    ],
     colorMode: {
-      defaultMode: 'light',
+      defaultMode: "light",
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
     navbar: {
-      title: 'Celest',
+      title: "Celest",
       logo: {
-        className: 'navbar-logo',
-        alt: 'Celest Logo',
-        src: 'img/logo.png',
+        className: "navbar-logo",
+        alt: "Celest Logo",
+        src: "img/logo.png",
       },
       items: [
-        process.env.NODE_ENV === 'development' && {
-          position: 'left',
-          type: 'doc',
-          to: 'docs/intro.md',
-          docId: 'intro',
-          label: 'Docs',
+        process.env.NODE_ENV === "development" && {
+          position: "left",
+          type: "doc",
+          to: "docs/intro.md",
+          docId: "intro",
+          label: "Docs",
         },
         {
-          docId: 'about-us',
-          to: '/about-us',
-          position: 'left',
-          label: 'About Us',
+          docId: "about-us",
+          to: "/about-us",
+          position: "left",
+          label: "About Us",
         },
         {
-          to: 'https://github.com/celest-dev/website',
+          to: "https://github.com/celest-dev/website",
           html: '<img src="/img/github.svg" class="navbar-custom-image" alt="GitHub Page" />',
-          position: 'right',
-          className: 'custom-navbar-img', 
+          position: "right",
+          className: "custom-navbar-img",
         },
         {
-          to: 'https://twitter.com/celest_dev',
+          to: "https://twitter.com/celest_dev",
           html: '<img src="/img/x.svg" class="navbar-custom-image" alt="X/Twitter Page" />',
-          position: 'right',
-          className: 'custom-navbar-img',
+          position: "right",
+          className: "custom-navbar-img",
         },
       ].filter(Boolean) as any,
     },
     footer: {
-      copyright:
-        `Copyright © ${new Date().getFullYear()} Celest`,
+      copyright: `Copyright © ${new Date().getFullYear()} Celest`,
     },
     prism: {
-      theme: prismThemes.github,
+      theme: prismThemes.oneLight,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['dart'],
+      additionalLanguages: ["dart"],
     },
   } satisfies Preset.ThemeConfig,
 };
