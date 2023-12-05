cbe7f6909c207fecdcf307739f2c9eece072ba9c
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-12-05 15:54:25 -0700

    feat: Add Sentry integration (#25)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,47 @@ const config: Config = {
     locales: ["en"],
   },
 
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://js.sentry-cdn.com',
+      }
+    },
+    {
+      tagName: 'script',
+      attributes: {
+        src: "https://js.sentry-cdn.com/5e1c9ebbaa5b3f636fc63ba5003412d2.min.js",
+        crossorigin: "anonymous",
+      },
+    },
+    {
+      tagName: 'script',
+      attributes: {},
+      innerHTML: `
+      Sentry.onLoad(function() {
+        Sentry.init({
+          attachStacktrace: true,
+          environment: "${process.env.NODE_ENV}",
+          integrations: [
+            new Sentry.BrowserTracing({
+              traceOrigins: ["localhost", "celest.dev"],
+              tracePropagationTargets: ["*"],
+            }),
+            new Sentry.Replay(),
+          ],
+          // Performance Monitoring
+          tracesSampleRate: 1.0, // Capture 100% of the transactions
+          // Session Replay
+          replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+          replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        });
+      });
+      `
+    }
+  ],
+
   presets: [
     [
       "classic",
