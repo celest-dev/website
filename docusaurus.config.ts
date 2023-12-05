bb8a499d656d6e7294ad41716ebd831d6bd0440f
Dillon Nys <dillon.andre.nys@gmail.com>
2023-12-05 16:46:01 -0700

    fix(sentry): Remove replay, reduce sample rate

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,13 +58,9 @@ const config: Config = {
               traceOrigins: ["*"],
               tracePropagationTargets: ["*"],
             }),
-            new Sentry.Replay(),
           ],
           // Performance Monitoring
-          tracesSampleRate: 1.0, // Capture 100% of the transactions
-          // Session Replay
-          replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-          replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+          tracesSampleRate: 0.5, // Capture 100% of the transactions
         });
       });
       `
