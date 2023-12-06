79270fd1ae21b97ce971c65d0c3a376a563facf3
Dillon Nys <dillon.andre.nys@gmail.com>
2023-12-06 14:02:31 -0700

    fix: Older browser issue
    Fixes uses of `??=` operator which is not available on older browsers.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+"use strict";
+
+// Fixes issues with older versions of Chrome in production,
+// e.g. https://celest.sentry.io/issues/4704582275/
+let targets;
+if (process.env.NODE_ENV === 'production') {
+  targets = { chrome: '58' };
+}
+
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  targets,
 };
