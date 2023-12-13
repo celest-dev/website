7e1e4377ea860e0bb589c11e200efea44528599d
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-12-13 16:06:08 -0700

    fix: Cannot destructure property 'scrollWidth' of 'c.current' as it is null. (#39)
    Fixes https://celest.sentry.io/issues/4723127744/

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 "use strict";
 
-// Fixes issues with older versions of Chrome in production,
-// e.g. https://celest.sentry.io/issues/4704582275/
+// Fixes issues with older browser versions in production,
+// - https://celest.sentry.io/issues/4704582275/
+// - https://celest.sentry.io/issues/4723127744/
 let targets = {};
 if (process.env.NODE_ENV === 'production') {
-  targets = { chrome: '58' };
+  targets = { chrome: '58', firefox: '16' };
 }
 
 module.exports = {
