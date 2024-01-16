b6cd0b7182bd0660b0bcdc9ef7c8797e8d0d04d7
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-01-16 09:53:29 -0800

    chore: Remove Google Analytics (#48)
    We have fully migrated to PostHog now, which is GDPR-compliant and more privacy-focused.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,10 +136,6 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
-        gtag: {
-          trackingID: "G-1FVDBFPGXN",
-          anonymizeIP: true,
-        },
       } satisfies Preset.Options,
     ],
   ],
