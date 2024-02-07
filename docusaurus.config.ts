b210822c7475de96f49592b043b008d3e68d6c74
Abdallah Shaban <ashaban@teo.cloud>
2024-02-07 13:36:34 -0800

    remove comments

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,8 +9,6 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://celest.dev",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
