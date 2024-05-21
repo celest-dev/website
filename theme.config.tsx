0ba123b1793c2f63a818c910711ff37ff5ea641e
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-05-20 22:03:30 -0700

    feat: Add Open Graph generation (#118)

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,50 +1,63 @@
-import React from 'react'
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
-import Logo from '@components/Logo'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import Logo from "@components/Logo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
-  logoLink: '/',
+  logoLink: "/",
   project: {
-    link: 'https://github.com/celest-dev/website',
+    link: "https://github.com/celest-dev/website",
   },
   chat: {
-    link: 'https://discord.gg/KtMAfSDXGQ',
+    link: "https://discord.gg/KtMAfSDXGQ",
   },
-  docsRepositoryBase: 'https://github.com/celest-dev/website',
+  docsRepositoryBase: "https://github.com/celest-dev/website",
   useNextSeoProps() {
-    return { titleTemplate: '%s | Celest' };
+    return { titleTemplate: "%s | Celest" };
   },
   head() {
     const { asPath } = useRouter();
     const url = `https://docs.celest.dev${asPath}`;
 
     const { frontMatter, title: defaultTitle } = useConfig();
-    const title = `${frontMatter.title ?? defaultTitle} | Celest`;
+    const title = frontMatter.title ?? defaultTitle;
+    const fullTitle = `${title} | Celest`;
+
+    const description = frontMatter.description || "The Flutter cloud platform";
+
+    let ogImage = "https://celest.dev/api/og";
+    if (asPath !== "/") {
+      ogImage += `?title=${encodeURIComponent(title)}`;
+    }
 
     return (
       <>
         <link rel="icon" type="image/png" href="/img/logo.png" />
-        <title>{title}</title>
+        <title>{fullTitle}</title>
         <meta property="og:url" content={url} />
-        <meta property="og:title" content={title} />
-        <meta
-          property="og:description"
-          content={frontMatter.description || 'The next site builder'}
-        />
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@celest_dev" />
+        <meta name="twitter:creator" content="@celest_dev" />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:image:alt" content={fullTitle} />
+        <meta name="twitter:title" content={fullTitle} />
+        <meta name="twitter:description" content={description} />
       </>
     );
   },
   search: {
-    placeholder: 'Search the docs...'
+    placeholder: "Search the docs...",
   },
   footer: {
-    text:  `Copyright © ${new Date().getFullYear()} Teo, Inc. (Celest)`,
+    text: `Copyright © ${new Date().getFullYear()} Teo, Inc. (Celest)`,
   },
   sidebar: {
     defaultMenuCollapseLevel: 2,
   },
-}
+};
 
-export default config
+export default config;
