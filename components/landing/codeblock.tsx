1c9ed081a5de15289c176b5a96140912a3345c62
Sheldon Curtiss <sheldon@celest.dev>
2024-10-06 20:33:13 -0700

    code block

--- components/landing/codeblock.tsx
+++ components/landing/codeblock.tsx
@@ -1,30 +1,33 @@
-import { Box, Code } from '@chakra-ui/react';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Highlight, themes } from "prism-react-renderer"
+import styles from 'styles.module.css'
 
-const CodeBlock = () => {
-  const code = `
-@cloud
+const codeBlock = `@cloud
 Future<String> sayHello(String name) async {
   return 'Hello, $name!';
-}`;
+}`
 
-  return (
-    <Box
-      backgroundColor="#1E1E1E" // Visual Studio Dark theme background
-      padding="4"
-      borderRadius="md"
-      overflow="auto"
-      whiteSpace="pre"
-    >
-      <Code
-        display="block"
-        whiteSpace="pre"
-        color="white"
-        fontFamily="Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace"
-      >
-        {code}
-      </Code>
-    </Box>
-  );
-};
+export const CodeBlock = () => (
+  <Highlight
+  theme={themes.vsDark} // Use the Visual Studio Dark theme
+  code={codeBlock}
+    language="dart"
+  >
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <pre style={style}>
+        {tokens.map((line, i) => (
+          <div key={i} {...getLineProps({ line })}>
+            <span>{i + 1}</span>
+            {line.map((token, key) => (
+              <span key={key} {...getTokenProps({ token })} />
+            ))}
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
+)
+
 
 export default CodeBlock;
